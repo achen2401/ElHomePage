e93b2a1f7899b3caacad980bcc07ae357bfe3c48
achen2401 <achen2401@gmail.com>
2015-10-16 09:39:16 -0700

    Adjusting CSS for smaller devices, adding more data

--- include/boo.js
+++ include/boo.js
@@ -84,16 +84,91 @@ function setImage(obj, resize) {
     $("#boo_dialog").dialog("open");
 }
 
+function adjustImageGalleryDV(){
+    var img = document.getElementById("galleryImage");
+    if (img) {
+        img.style.height = (parseInt(window.innerHeight/2)) + parseInt(window.innerHeight/12)  + "px";
+        img.style.width = (parseInt(window.innerWidth/2) + parseInt(window.innerWidth/4))+ "px";
+    }
+
+}
+
 function handleClick(obj, category) {
+    //alert(category)
     switch(category) {
         case "labMembers":
             document.getElementById('labImage').src= obj.getAttribute('imgsrc');
-            var itemId = obj.getAttribute("itemid");
-            document.getElementById("labImageArrow:" + itemId).style.display = "inline";
-            var images = document.getElementsByName("labImageArrow");
+            //var itemId = obj.getAttribute("itemid");
+            //document.getElementById("labImageArrow:" + itemId).style.display = "inline";
+            //var images = document.getElementsByName("labImageArrow");
             //alert(itemId)
-            for (var i = 0; i < images.length; i++){
-                if (images[i].getAttribute("itemid") != itemId) images[i].style.display = "none";
+            //for (var i = 0; i < images.length; i++){
+               // if (images[i].getAttribute("itemid") != itemId) images[i].style.display = "none";
+            //}
+            break;
+        case "imageGallery":
+            //alert("window innerHeight: " + window.innerHeight + " window innerWidth: " + window.innerWidth)
+            var arrowId = obj.getAttribute("id");
+            var img = document.getElementById("galleryImage");
+            var id = img.getAttribute("itemid");
+            var imgTitle = document.getElementById("galleryImageTitle");
+
+            //adjustImageGalleryDV();
+            //alert(arrowId)
+            if (arrowId == "galleryLeftArrow") {
+                id = parseInt(id) - 1;
+                if (id > 0) {
+                    //if (id != 0) {
+                        img.setAttribute("itemid", id);
+                        img.src = arrGalleryImages[id].imgsrc;
+                        imgTitle.innerHTML = arrGalleryImages[id].title;
+                        img.style.height = parseInt(window.innerHeight/2) * parseInt(arrGalleryImages[id].hfactor);
+                        img.style.width = parseInt(window.innerWidth/2) * parseInt(arrGalleryImages[id].wfactor);
+                        obj.style.opacity = 1;
+
+                    //}
+
+                } else {
+                    img.setAttribute("itemid", "0");
+                    img.src = arrGalleryImages[0].imgsrc;
+                    imgTitle.innerHTML = arrGalleryImages[0].title;
+                    img.style.height = parseInt(window.innerHeight/2) * parseInt(arrGalleryImages[0].hfactor);
+                    img.style.width = parseInt(window.innerWidth/2) * parseInt(arrGalleryImages[0].wfactor);
+
+                    obj.style.opacity = 0.7;
+                    
+                }
+
+                document.getElementById("galleryRightArrow").style.opacity = 1;
+            } else {
+                id = parseInt(id) + 1;
+                //alert(arrGalleryImages[id].title)
+                if (id < (arrGalleryImages.length - 1)) {
+                    //alert("h:" + parseInt(arrGalleryImages[id].hfactor) + " w: " + parseInt(arrGalleryImages[id].wfactor))
+                    //if (id != 0) {
+                        img.setAttribute("itemid", id);
+                        img.src = arrGalleryImages[id].imgsrc;
+                        imgTitle.innerHTML = arrGalleryImages[id].title;
+                        obj.style.opacity = 1;
+                        img.style.height = parseInt(window.innerHeight/2) * parseInt(arrGalleryImages[id].hfactor);
+                        img.style.width = parseInt(window.innerWidth/2) * parseInt(arrGalleryImages[id].wfactor);
+
+                    //}
+
+                } else {
+                    //alert("h:" + parseInt(arrGalleryImages[(arrGalleryImages.length - 1)].hfactor) + " w: " + parseInt(arrGalleryImages[(arrGalleryImages.length - 1)].wfactor))
+                    img.setAttribute("itemid", (arrGalleryImages.length - 1));
+                    img.src = arrGalleryImages[(arrGalleryImages.length - 1)].imgsrc;
+                    imgTitle.innerHTML = arrGalleryImages[(arrGalleryImages.length - 1)].title;
+                    img.style.height = parseInt(window.innerHeight/2) * parseInt(arrGalleryImages[(arrGalleryImages.length - 1)].hfactor);
+                    img.style.width = parseInt(window.innerWidth/2) * parseInt(arrGalleryImages[(arrGalleryImages.length - 1)].wfactor);
+
+                    obj.style.opacity = 0.7;
+                    
+                }
+
+                document.getElementById("galleryLeftArrow").style.opacity = 1;
+
             }
 
 
@@ -101,6 +176,7 @@ function handleClick(obj, category) {
 
 }
 var mainApp = angular.module("mainApp", ["ngRoute"]);
+var arrGalleryImages = new Array();
 
 mainApp.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.
@@ -162,6 +238,7 @@ mainApp.controller('booController', function ($scope, $http) {
     });
     $http.get(imageGalleryPath).success(function (response) {
         $scope.imageGallery = response;
+        arrGalleryImages = response;
     });
     $http.get(categoriesPath).success(function (response) {
         $scope.categories = response;
@@ -169,9 +246,18 @@ mainApp.controller('booController', function ($scope, $http) {
 
 
 
-    $scope.defaultLabImage = "./images/wholelab-new.jpg"
+    $scope.defaultLabImage = "./images/wholelab-new.jpg";
+    $scope.defaultImageGallery = "./images/gallery1.jpg";
+    $scope.defaultImageGalleryTitle = "Engrafted ERMS tumor in the cranium from transplantation";
+    //$scope.adjustImageGalleryDV = function() {
 
-    
+       // adjustImageGalleryDV();
+    //}
+
+    $scope.defaultImageGalleryHeight = (parseInt(window.innerHeight/2)) + parseInt(window.innerHeight/16)  + "px";
+    $scope.defaultImageGalleryWidth = (parseInt(window.innerWidth/2) + parseInt(window.innerWidth/9))+ "px";
+
+
     $scope.boo = {
     title: "Eleanor Chen, MD, PHD",
     institution: "University of Washington Medical Center",
